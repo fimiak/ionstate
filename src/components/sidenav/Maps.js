d2be38f809a1c98a14c0716cdc0cc9f16ca84e8e
James Greve <jgreve@livescribe.com>
2017-05-31 11:37:54 -0700

    Added places for maps

--- src/components/sidenav/Maps.js
+++ src/components/sidenav/Maps.js
@@ -2,10 +2,46 @@ import React, {Component} from 'react';
 
 class Maps extends Component {
   render() {
+    let place = '';
+    let data = '';
+    switch (this.props.params.country) {
+      case 'usa':
+        place = 'White+House,Washington,USA';
+        break;
+      case 'uk':
+        place = '10+downing+street,London,UK';
+        break;
+      case 'mexico':
+        place = 'los+pinos,ciudad+de+mexico,distrito+federal';
+        break;
+      case 'france':
+        place = 'Elysee+Palace,Paris,France';
+        break;
+      case 'germany':
+        place = 'Reichstag+Building,Berlin,Germany';
+        break;
+      case 'canada':
+        place = '24+sussex+drive,ottawa,canada';
+        break;
+      case 'russia':
+        place = 'The+Kremlin,Moscow+Russia';
+        break;
+      case 'china':
+        place = 'zhongnanhai,Beijing,China';
+        break;
+      default:
+        place = 'The+World';
+        break;
+    }
     return (
       <div>
+        <h4>Map {this.props.sel}</h4>
         <div>
-          <iframe className="maps" src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d196881.35386386706!2d-122.22023373657828!3d37.66825555575328!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1490305617754" frameBorder="0" allowFullScreen></iframe>
+          <iframe
+            className="maps"
+            frameBorder="0"
+            src={"https://www.google.com/maps/embed/v1/place?key=AIzaSyBEERsCbyJvSuyQFpq2IcnunlrO-m-qhc0&q=" + place} allowFullScreen>
+</iframe>
         </div>
       </div>
     )
