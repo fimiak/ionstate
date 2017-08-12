0d1ab81f5bb1ed2044f62f75ec98c28d97267781
James Greve <jgreve@livescribe.com>
2017-08-11 17:29:15 -0700

    Updates to PageMenu component and representing the countries in a cleaner fashion

--- src/components/sidenav/NavWrap.js
+++ src/components/sidenav/NavWrap.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NavLink from '../NavLink';
+import PageMenu from './PageMenu';
 import data from '../../data/data';
 import clearIcon from '../../icons/x-circle.svg';
 import forwardIcon from '../../icons/arrow-right.svg';
@@ -33,7 +34,6 @@ class NavWrap extends Component {
         document.getElementById('page1').setAttribute('style', 'display: flex;');
         document.getElementById('page2').setAttribute('style', 'display: none;');
         document.getElementById('page3').setAttribute('style', 'display: none;');
-        
     }
 
     setNation(nation) {
@@ -81,75 +81,36 @@ class NavWrap extends Component {
     }
 
     render() {
-        
+
         return (
           <div id="nav-wrap" className={"nav-wrap " + this.props.menuState}>
             <div className="inner-header" >
               <div className="nav-sidenav" onClick={this.props.closeMenu}>
                 <ul id="page1" onClick={this.scrollTop}>
-                  <li><NavLink to="'/' + {country[0]}" onClick={() => this.setNation(country[0])}>
-                    <img src={country[0].thumb} alt={country[0].leader} />
-                    <div className="copy" id="copy">{country[0].leader}</div></NavLink></li>
-                  <li><NavLink to="/uk" onClick={() => this.setNation(country[1])}>
-                    <img src={country[1].thumb} alt={country[1].leader} />
-                    <div className="copy">{country[1].leader}</div></NavLink>
-                  </li>
-                  <li><NavLink to="/mexico" onClick={() => this.setNation(country[2])}>
-                    <img src={country[2].thumb} alt={country[2].leader} />
-                    <div className="copy">{country[2].leader}</div></NavLink>
-                  </li>
-                  <li><NavLink to="/france" onClick={() => this.setNation(country[3])}>
-                    <img src={country[3].thumb} alt={country[3].leader} />
-                    <div className="copy">{country[3].leader}</div></NavLink>
-                  </li>
-                  <li><NavLink to="/germany" onClick={() => this.setNation(country[4])}>
-                    <img src={country[4].thumb} alt={country[4].leader} />
-                    <div className="copy">{country[4].leader}</div></NavLink></li>
-                  <li><NavLink to="/canada" onClick={() => this.setNation(country[5])}>
-                    <img src={country[5].thumb} alt={country[5].leader} />
-                    <div className="copy">{country[5].leader}</div></NavLink></li>
-                  <li><NavLink to="/russia" onClick={() => this.setNation(country[6])}>
-                    <img src={country[6].thumb} alt={country[6].leader} />
-                    <div className="copy">{country[6].leader}</div></NavLink></li>
-                  <li><NavLink to="/china" onClick={() => this.setNation(country[7])}>
-                    <img src={country[7].thumb} alt={country[7].leader} />
-                    <div className="copy">{country[7].leader}</div></NavLink></li>
-                  <li><NavLink to="/japan" onClick={() => this.setNation(country[8])}>
-                    <img src={country[8].thumb} alt={country[8].leader} />
-                    <div className="copy">{country[8].leader}</div></NavLink>
-                  </li>
+                    <PageMenu country={country} page={1} />
                 </ul>
                 <ul id="page2" onClick={this.scrollTop}>
-                  <li><NavLink to="/brazil" onClick={() => this.setNation(country[9])}><img src={country[9].thumb} alt={country[9].leader} /><div className="copy">{country[9].leader}</div></NavLink></li>
-                  <li><NavLink to="/india" onClick={() => this.setNation(country[10])}><img src={country[10].thumb} alt={country[10].leader} /><div className="copy">{country[10].leader}</div></NavLink></li>
-                  <li><NavLink to="/australia" onClick={() => this.setNation(country[11])}><img src={country[11].thumb} alt={country[11].leader} /><div className="copy">{country[11].leader}</div></NavLink></li>
-                  <li><NavLink to="/europeanunion" onClick={() => this.setNation(country[12])}><img src={country[12].thumb} alt={country[12].leader} /><div className="copy">{country[12].leader}</div></NavLink></li>
-                  <li><NavLink to="/argentina" onClick={() => this.setNation(country[13])}><img src={country[13].thumb} alt={country[13].leader} /><div className="copy">{country[13].leader}</div></NavLink></li>
-                  <li><NavLink to="/indonesia" onClick={() => this.setNation(country[14])}><img src={country[14].thumb} alt={country[14].leader} /><div className="copy">{country[14].leader}</div></NavLink></li>
-                  <li><NavLink to="/italy" onClick={() => this.setNation(country[15])}><img src={country[15].thumb} alt={country[15].leader} /><div className="copy">{country[15].leader}</div></NavLink></li>
-                  <li><NavLink to="/southkorea" onClick={() => this.setNation(country[16])}><img src={country[16].thumb} alt={country[16].leader} /><div className="copy">{country[16].leader}</div></NavLink></li>
-                  <li><NavLink to="/saudiarabia" onClick={() => this.setNation(country[17])}><img src={country[17].thumb} alt={country[17].leader} /><div className="copy">{country[17].leader}</div></NavLink></li>
+                    <PageMenu country={country} page={2} />
                 </ul>
                 <ul id="page3" onClick={this.scrollTop}>
-                    <li><NavLink to="/turkey" onClick={() => this.setNation(country[18])}><img src={country[18].thumb} alt={country[18].leader} /><div className="copy">{country[18].leader}</div></NavLink></li>
-                    <li><NavLink to="/southafrica" onClick={() => this.setNation(country[19])}><img src={country[19].thumb} alt={country[19].leader} /><div className="copy">{country[19].leader}</div></NavLink></li>
-                    <li><NavLink to="/unitedarabemirates" onClick={() => this.setNation(country[20])}><img src={country[20].thumb} alt={country[20].leader} /><div className="copy">{country[20].leader}</div></NavLink></li>
-                    <li><NavLink to="/egypt" onClick={() => this.setNation(country[21])}><img src={country[21].thumb} alt={country[21].leader} /><div className="copy">{country[21].leader}</div></NavLink></li>
-                    <li><NavLink to="/vietnam" onClick={() => this.setNation(country[22])}><img src={country[22].thumb} alt={country[22].leader} /><div className="copy">{country[22].leader}</div></NavLink></li>
-                    <li><NavLink to="/iran" onClick={() => this.setNation(country[23])}><img src={country[23].thumb} alt={country[23].leader} /><div className="copy">{country[23].leader}</div></NavLink></li>
-                    <li><NavLink to="/nigeria" onClick={() => this.setNation(country[24])}><img src={country[24].thumb} alt={country[24].leader} /><div className="copy">{country[24].leader}</div></NavLink></li>
-                    <li><NavLink to="/pakistan" onClick={() => this.setNation(country[25])}><img src={country[25].thumb} alt={country[25].leader} /><div className="copy">{country[25].leader}</div></NavLink></li>
-                    <li><NavLink to="/thailand" onClick={() => this.setNation(country[26])}><img src={country[26].thumb} alt={country[26].leader} /><div className="copy">{country[26].leader}</div></NavLink></li>
+                    <PageMenu country={country} page={3} />
                 </ul>
               </div>
               <div className="nav-links">
                 <ul>
-                  <li><NavLink activeClassName="active"><img className="img-size" src={barIcon} alt="gdp" />Filter By GDP</NavLink></li>
-                  <li><NavLink activeClassName="active"><img className="img-size" src={usersIcon} alt="population" />Filter By Population</NavLink></li>
-                  <li><NavLink activeClassName="active"><img className="img-size" src={trendIcon} alt="growth" />Filter By Growth</NavLink></li>
-                  <li><NavLink to='/' activeClassName="active"><img className="img-size" src={clearIcon} alt="about" />About</NavLink></li>                  
-                  <li><NavLink activeClassName="active" onClick={this.changePage}><img className="img-size" src={forwardIcon} alt="next" />Next Page</NavLink></li>
-                  <li><NavLink><img className="img-size" src={alignLeft} alt="page" />Page {this.state.page}</NavLink></li>
+                  <li><NavLink activeClassName="active">
+                      <img className="img-size" src={barIcon} alt="filter gdp" />Filter By GDP</NavLink></li>
+                  <li><NavLink activeClassName="active">
+                      <img className="img-size" src={usersIcon} alt="filter population" />Filter By Population</NavLink></li>
+                  <li><NavLink activeClassName="active">
+                      <img className="img-size" src={trendIcon} alt="filter growth" />Filter By Growth</NavLink></li>
+                  <li><NavLink to='/' activeClassName="active">
+                      <img className="img-size" src={clearIcon} alt="About" />About</NavLink></li>  
+                  <li><NavLink activeClassName="active" onClick={this.changePage}>
+                      <img className="img-size" src={forwardIcon} alt="Next Page" />Next Page</NavLink></li>
+                  <li><NavLink>
+                      <img className="img-size" src={alignLeft} alt="Page Number" />Page {this.state.page}</NavLink>
+                  </li>
                 </ul>
               </div>
             </div>
---
+++ src/components/sidenav/PageMenu.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import NavLink from '../NavLink';
+
+function PageMenu(country, page) {
+    let page1 = [];
+    let page2 = [];
+    let page3 = [];
+    
+    const listItems = country.country.map((country) =>
+        <li key={country.id.toString()}>
+            <NavLink to={country['country']} onClick={() => this.setNation(country['country'])}>
+                <img src={country.thumb} alt={country.leader} />
+                <div className="copy">{country.leader}</div>
+            </NavLink>
+        </li>
+    );
+
+    listItems.forEach(function(item) {
+        if (item.key < 9) {
+            page1.push(item);
+        } else if (item.key > 9 && item.key < 19) {
+            page2.push(item);
+        } else {
+            page3.push(item);
+        }
+    })
+
+    return (
+        <div>
+            {
+                country.page === 1 &&
+                <ul>{page1}</ul>
+            }
+            {
+                country.page === 2 &&
+                <ul>{page2}</ul>
+            }
+            {
+                country.page === 3 &&
+                <ul>{page3}</ul>
+            }
+        </div>
+    )
+}
+
+
+export default PageMenu;
