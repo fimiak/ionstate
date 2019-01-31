99d22c85cd286648acd01dfdac32bc077ae7b2fc
Tyler James Greve <tylerjamesgreve@gmail.com>
2019-01-30 19:41:29 -0800

    Made changes to improve the look of the page before the initial fetch has loaded

--- src/components/main/home/Home.js
+++ src/components/main/home/Home.js
@@ -4,63 +4,7 @@ import Featured from '.././home/featured/Featured';
 import Title from '.././home/title/Title';
 
 class Home extends Component {
-  state = {
-    news: [
-      {
-        response: {
-          docs: [
-            {
-              id: 0,
-              headline: {
-                main: 'Loading'
-              },
-              keywords: [
-                {
-                  isMajor: 'N',
-                  rank: 1,
-                  name: 'glocations',
-                  value: 'Belarus'
-                },
-                {
-                  isMajor: 'N',
-                  rank: 1,
-                  name: 'persons',
-                  value: 'Lukashenko'
-                }
-              ],
-              leader: '5b7f07a3fb6fc0183b3fc1a1',
-              byline: {
-                original: 'By REUTERS'
-              },
-              multimedia: [
-                {
-                  url: 'testdata/test2.jpg'
-                },
-                {
-                  url: 'testdata/test2.jpg'
-                }
-              ],
-              pub_date: '2017-08-12T12:21:27+0000',
-              snippet: '',
-              source: '',
-              country: '',
-              web_url: ''
-            }
-          ]
-        }
-      }
-    ],
-    nations: [
-      {
-        2018: [
-          {
-            election: 'Zimbabwe',
-            date: '7/29/2018'
-          }
-        ]
-      }
-    ]
-  };
+  state = {};
 
   componentDidMount() {
     fetch('https://fast-journey-76120.herokuapp.com/api/news')
@@ -69,6 +13,11 @@ class Home extends Component {
   }
 
   render() {
+    /**** CHECK */
+    if (!this.state.news) {
+      return <Title title="IONSTATE:" subtitle="The biggest daily news and topics concerning today's world leaders" />;
+    }
+    /*** */
     let selection = Math.floor(Math.random() * 30);
     switch (selection) {
       case 0:
